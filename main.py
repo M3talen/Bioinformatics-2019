82f8025dff84ed4abba8bb7dc89f109e61657575
ZvonimirKucis <zvonimirkucis@gmail.com>
2019-12-05 22:30:40 +0100

    simple testing inside main.py

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ data = fh.read(input_file)
 alg = local.SmithWaterman(2, -1, -2)
 
 start = time.time()
-for seqA in data:
-    for seqB in data[::-1]:
+for i, seqA in enumerate(data[0:100]):
+    for seqB in data[i+1:100]:
         _ = alg.score(seqA['seq'], seqB['seq'])
 end = time.time()
 print(f'Execution time: {(end - start)} s')
