6b8c2a069eb3ea54346b33af4071e23c2bbee377
ZvonimirKucis <zvonimirkucis@gmail.com>
2020-01-09 22:21:33 +0100

    calculating distance matrix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import file_handler as fh 
-from algorithms import local_aligment as local
-from clustering import MSC
+from algorithms import global_aligment as globalAlg
 
 import time
 import sys
@@ -8,12 +7,25 @@ import sys
 input_file = sys.argv[1]
 
 data = fh.read(input_file)
-
-alg = local.SmithWaterman(2, -1, -2)
+alg = globalAlg.NeedlemanWunsch()
+print(len(data))
+distance_matrix = []
 
 start = time.time()
-for i, seqA in enumerate(data[0:100]):
-    for seqB in data[i+1:100]:
-        _ = alg.score(seqA['seq'], seqB['seq'])
+for i, seqA in enumerate(data[0:40]):
+    distance_matrix.append([])
+    for seqB in data[0:40]:
+        score = alg.run(seqA['seq'], seqB['seq'])
+        distance_matrix[i].append(score)
 end = time.time()
 print(f'Execution time: {(end - start)} s')
+
+print('\nDistance matrix:')
+for i in range(len(distance_matrix)):
+    #print('\n')
+    print('\t[', end = '')
+    for j in range(len(distance_matrix)):
+        d = distance_matrix[i][j]
+        print("%5d"%(d), end = '')
+    print(']')
+
