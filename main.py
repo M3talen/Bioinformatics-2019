5bbc246c4a031b757acefb805c6e354993ec2440
ZvonimirKucis <zvonimirkucis@gmail.com>
2020-01-10 11:43:07 +0100

    applied hierarchical clustering

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import file_handler as fh 
 from algorithms import global_aligment as globalAlg
+from clustering import hiearhical
 
 import time
 import sys
+import numpy as np
 
 input_file = sys.argv[1]
 
@@ -11,7 +13,7 @@ data = fh.read(input_file)
 alg = globalAlg.NeedlemanWunsch()
 print(f"data length: {len(data)}")
 
-N = 50
+N = len(data)
 distance_matrix = []
 for i in range(len(data[0:N])):
     distance_matrix.append([None] * N)
@@ -25,16 +27,58 @@ for i, seqA in enumerate(data[0:N]):
             score = alg.run(seqA['seq'], seqB['seq'])
             distance_matrix[i][j] = score
 end = time.time()
-print(f'Execution time: {(end - start)} s')
+print(f'Execution time (distance matrix): {(end - start)} s')
 
-print('\nDistance matrix:')
-for i in range(len(distance_matrix)):
-    print('\t[', end = '')
-    for j in range(len(distance_matrix[i])):
-        d = distance_matrix[i][j]
-        if(d == None):
-            d = 0
-        print("%5d"%(d), end = '')
-    print(']')
+#print('\nDistance matrix:')
+#for i in range(len(distance_matrix)):
+#    print('\t[', end = '')
+#    for j in range(len(distance_matrix[i])):
+#        d = distance_matrix[i][j]
+#        if(d == None):
+#            d = 0
+#        print("%5d"%(d), end = '')
+#    print(']')
 
 print(f"shape: ({len(distance_matrix)},{len(distance_matrix[0])})")
+
+clustering = hiearhical.HierarchicalClustering()
+start = time.time()
+clustering.fit(distance_matrix)
+end = time.time()
+print(f'Execution time (cluster fit): {(end - start)} s')
+#print(clustering.get_labels())
+cluster_labels = clustering.get_labels()
+
+start = time.time()
+cluster_dict = {}
+for i in range(len(cluster_labels)):
+    label = cluster_labels[i]
+    if label not in cluster_dict:
+        cluster_dict[label] = []
+    cluster_dict[label].append((i,data[i]))
+end = time.time()
+print(f'Execution time (grouping clusters): {(end - start)} s')
+#print(cluster_dict)
+beta = 1
+centorids = []
+start = time.time()
+for label in cluster_dict:
+    cluster = cluster_dict[label]
+    print(f"Cluster {label}, length: {len(cluster)}")
+    distances = []
+    for b, i in enumerate(cluster):
+        distances.append([])
+        for j in cluster:
+            x = i[0]
+            y = j[0]
+            distances[b].append(distance_matrix[x][y])
+
+    distances = np.asarray(distances)
+    index = np.exp(-beta*distances / (distances.std() + 0.00000001)).sum(axis=1).argmax()
+    centorids.append(cluster[index][1])
+end = time.time()
+print(f'Execution time (calcualting centorids): {(end - start)} s')
+print("\nCentroids:")
+print(centorids)
+
+fh.write(centorids, 'output.fasta')
