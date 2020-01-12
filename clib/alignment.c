ea05fbfaed176b34078247f23462da4d1ce7c1f3
zlatkoverk <zlatko112@gmail.com>
2020-01-12 12:15:36 +0100

    working c functions for alignment

--- clib/alignment.c
+++ clib/alignment.c
@@ -19,15 +19,6 @@ int find_maximum(int p[], int n)
     return max;
 }
 
-void swap(char *str1, char *str2) 
-{ 
-  char *temp = (char *)malloc((strlen(str1) + 1) * sizeof(char)); 
-  strcpy(temp, str1); 
-  strcpy(str1, str2); 
-  strcpy(str2, temp); 
-  free(temp); 
-}
-
 void print_matrix(int *matrix, int rows, int cols){
     for(int i = 0; i < rows; i++) {
         printf("\n");
@@ -41,12 +32,14 @@ void print_matrix(int *matrix, int rows, int cols){
 int global_score(char genA[], char genB[]){
     int rows = strlen(genA) + 1;
 	int cols = strlen(genB) + 1;
+
     int *matrix = calloc(rows * cols, sizeof(int));
     for(int i = 0; i < rows; i++)
         *(matrix + i*cols) = GAP * i;
     
     for(int j = 0; j < cols; j++)
         *(matrix + j) = GAP * j;
+
     int values[3] = {0};
     int score;
     for(int i = 1; i < rows; i++) {
@@ -69,56 +62,33 @@ int global_score(char genA[], char genB[]){
     return res;
 }
 
-int local_score(char strA[],  char strB[]){
-    int lenA = strlen(strA) + 1;
-	int lenB = strlen(strB) + 1;
-    if(lenA > lenB){
-        swap(strA, strB);
-        lenA = strlen(strA) + 1;
-        lenB = strlen(strB) + 1;
-    }
+int local_score(char genA[],  char genB[]){
+    int rows = strlen(genA) + 1;
+	int cols = strlen(genB) + 1;
 
-    int *matrix = calloc(lenA * lenB, sizeof(int));
-
-	//Create empty table
-	for(int i=0;i<lenB;++i){
-		*(matrix + i*lenB)=0;
-	}
-	for(int j=0;j<lenA;j++){
-		*(matrix + j) = 0;
-	}
-
+    int *matrix = calloc(rows * cols, sizeof(int));
+    
     int max = 0;
     int values[4] = {0};
     int score;
-    for(int i = 1; i < lenA; ++i) {
-		for(int j = 1; j < lenB; ++j) {
-            if(strA[i-1] == strB[j-1]){
+    for(int i = 1; i < rows; ++i) {
+		for(int j = 1; j < cols; ++j) {
+            if(genA[i-1] == genB[j-1]){
                 score = MATCH;
             }else{
                 score = MISMATCH;
             }
 
-            values[0] = *(matrix + (i-1)*lenB + j) + GAP;
-            values[1] = *(matrix + i*lenB + j-1) + GAP;
-            values[2] = *(matrix + (i-1)*lenB + j-1) + score;
+            values[0] = *(matrix + (i-1)*cols + j) + GAP;
+            values[1] = *(matrix + i*cols + j-1) + GAP;
+            values[2] = *(matrix + (i-1)*cols + j-1) + score;
 
-            *(matrix + i*lenB + j) = find_maximum(values, 4);
-            if(*(matrix + i*lenB + j) > max){
-                max = *(matrix + i*lenB + j);
+            *(matrix + i*cols + j) = find_maximum(values, 4);
+            if(*(matrix + i*cols + j) > max){
+                max = *(matrix + i*cols + j);
             }
         }
     }
     free(matrix);
     return max;
 }
-
-//int main(){
-//    char *a = "GTCGA";
-//    char *b = "GAGAGA";
-//
-//    int score = global_score(a, b);
-//    printf("score: %d\n", score);
-//    printf("hello world");
-//    return 0;
-//}
